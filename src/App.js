688313cce840d7c624df127b9f7221818d96b519
greeshmasunil10 <greeshmasunil10@gmail.com>
2023-02-15 12:26:36 -0500

    changed text, added descr, changed colorsd

--- src/App.js
+++ src/App.js
@@ -32,8 +32,9 @@ function App() {
     <div className="sample">
       <form className="form" onSubmit={handleSubmit}>
         <label className="label">
-          <div className="banner">Holy Guidance</div>
-          <p>{GREETING_QUESTION} </p>
+          <div className="banner">HOLY GUIDANCE</div>
+          <p className="description">Get biblical guidance for your problems and worries.</p>
+          <p className="greeting-qsn">{GREETING_QUESTION} </p>
           <br />
           <textarea
             className="input"
@@ -44,13 +45,14 @@ function App() {
           />
         </label>
         <button className="button" type="submit">
-          {isLoading ? <div className="loader"></div> : "Ask"}
+          {isLoading ? <div className="loader"></div> : "Ask Bible Buddy"}
         </button>
       </form>
       {error && <p className="error">{error}</p>}
 
       {response && (
         <div className="message-box">
+          <p style={{fontWeight:"bold"}}>Bible Buddy says...</p>
           <p className="message-text">{response}</p>
         </div>
       )}
